da71319ab2993aa316ebaa7f498d385179288462
ricardo <ricardobboy_@hotmail.com>
2019-07-09 22:08:31 -0300

    Corrigido erro ao passar o usuario para model pois ainda não se tem usuario logado e foi passado o valor de null

--- api/src/controller/LoginController.php
+++ api/src/controller/LoginController.php
@@ -18,7 +18,7 @@ class LoginController {
     private $jwtKey;
 
     public function __construct($container) {
-        $this->userModel = new UserModel($container->get(EntityManager::class), $this->user); 
+        $this->userModel = new UserModel($container->get(EntityManager::class), null); 
         $this->itemsPerPage = $container->get('config')['app']['itemsPerPageDefault'];
         $this->dirPicuture = $container->get('dirPicuture');
         $this->jwtKey = $container->get('jwtKey');
